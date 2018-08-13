b04d226f0b3c409cbe43e7f9e79a559591eb24ff
EllenZhangYarong <ellenzhg@hotmail.com>
2018-08-14 11:04:36 +1200

    Tabbed Page studying - youtube

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # 100DaysSwift
+
+## 2018-08-14
+- Learn about the <em>UITABCONTROLLER</em>> and how to use it
+- Learn how to combine <em>TAB CONTROLLERS</em>> and <em>NAVIGATION CONTROLLERS</em>
+- Learn how to change the <em>STARTUP TAB</em> in a <em>TAB CONTROLLER</em>
+
+
---
+++ TabbedPageStudy/TabbedPageStudy/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TabbedPageStudy
+//
+//  Created by 张亚荣 on 2018/8/14.
+//  Copyright © 2018年 EllenAndAngel. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
